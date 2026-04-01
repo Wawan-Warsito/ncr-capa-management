0bba4e535267c3906ecd28114e67088cb91691cb
Wawan-Warsito <wawanwarsito87@gmail.com>
2026-04-01 23:23:49 +0700

    fix: stabilize CI and prevent SPA from handling /api

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,8 @@ use App\Http\Controllers\PublicController;
 Route::get('/share/ncrs/{id}/print', [PublicController::class, 'print']);
 
 Route::fallback(function () {
+    if (request()->is('api') || request()->is('api/*')) {
+        abort(404);
+    }
     return view('welcome');
 });
